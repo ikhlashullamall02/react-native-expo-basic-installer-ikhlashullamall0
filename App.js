fbd1fb5c48866d17f996d38422a8a7bafe8355b5
iklashullamall02 <105841105220@student.unismuh.ac.id>
2023-08-10 10:47:58 +0800

    component,image,and button

--- App.js
+++ App.js
@@ -1,52 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
-import tandaPanah from './assets/Vector.png'
-import facebook from './assets/facebook.png'
-import google from './assets/google.png'
-
-export default function App() {
+import { StyleSheet, Text, View , SafeAreaView } from 'react-native'
+import React from 'react'
+import { useFonts } from "expo-font";
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    "Inter-Black": require("./assets/font/Inter-Black-900.otf"),
+    "Inter-Italic": require("./assets/font/Inter-Black-Italic-900.otf"),
+  });
+  if (!fontsLoaded) {
+    return <Text>Memuat Font</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View >
-        <Text style={{
-          fontSize: 34,
-          fontWeight: 'bold'
-        }}>forgot password</Text>
-        <Text 
-        style={{
-          marginTop: 20
-        }}
-        >
-            pleas, Enter your email address, you will receive a link to create a new password via email,
-            </Text> 
-        <TextInput style={{
-          height: 64,
-          width: 320,
-          borderColor: 'gray',
-          borderWidth: 1,
-          marginTop: 20,
-          borderRadius: 10,
-          paddingLeft: 10,
-          marginBottom: 20
-        }} placeholder="Email" />
-        <Button style={{
-          marginTop: 40,
-        }} title="send" />
-
-
-        </View>
-    </View>
-  );
+    <SafeAreaView>
+      <View styles={styles.container}>
+        <Text style={{ fontFamily: "Inter-Black" }}>Inter Black</Text>
+        <Text style={{ fontFamily: "Inter-Italic" }}>Inter Italic</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
-
+export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    paddingTop: 100,
-    paddingLeft: 20,
-  },
-});
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 30,
+    paddingTop: 55,
+  }
+})
